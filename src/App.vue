f26c4c4a52f06b2f9e40db6b68ea51544b1dc2b8
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-06-15 22:39:56 -0300

    add Nav component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import Characters from './components/Characters.vue'
+import Nav from './components/Nav.vue';
 
 </script>
 
 <template>
+  <Nav/>
   <Characters/>
 </template>
 
---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <nav class="nav">
+        <p class="nav__p">home</p>
+    </nav>
+</template>
+
+
+<style scoped>
+    .nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px;
+    }
+    nav__p {
+        font-size: 20px;
+        font-weight: bold;
+    }
+</style>
